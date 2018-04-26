339ddfdbdabbdde63124375d765eea3bee0df3fb
Manuela Solís <manuelaymanuela@hotmail.com>
2018-04-26 19:57:25 +0200

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Este archivo debería contener toda la creación de registros necesarios para descargar la base de datos con sus valores por defecto. 
+# Los datos pueden cargarse después del comando rails db:seed (también pueden crearse al mismo tiempo que la base de datos con db:setup). 
+#
+# Ejemplos:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'El señor de los anillos' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+
+categories = Category.create([{name: "Neclaces"}, {name: "Rings"}, 
+    {name: "Earrings"}, {name: "T-Shirts"}, {name: "Otro"}])
